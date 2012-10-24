f322be3eda7b3ac08ec8be6d3f86af4552203727
Ric Allinson <capecodehq@yahoo.co.uk>
2012-10-24 00:01:15 -0700

    Added comments and cleaned up ideas

--- examples/basic/main.js
+++ examples/basic/main.js
@@ -2,27 +2,40 @@
 
 "use strict";
 
-define(["lib/canvas", "lib/gameloop"], function (canvas, GameLoop) {
+define(["lib/canvas", "lib/engine"], function (canvas, Engine) {
 
-    var loop = new GameLoop(canvas);
+    var engine = new Engine(canvas);
+
+    // // http://paulirish.com/2011/requestanimationframe-for-smart-animating
+    // // shim layer with setTimeout fallback
+    // window.requestAnimFrame = (function () {
+    //     return window.requestAnimationFrame ||
+    //         window.webkitRequestAnimationFrame ||
+    //         window.mozRequestAnimationFrame ||
+    //         window.oRequestAnimationFrame ||
+    //         window.msRequestAnimationFrame ||
+    //         function (callback) {
+    //             window.setTimeout(callback, 1000 / 60);
+    //         };
+    // }());
 
     window.addEventListener("keydown", function (e) {
         e.preventDefault();
-        loop.changeKeyPressed(e.keyCode, true);
+        engine.changeKeyPressed(e.keyCode, true);
     }, false);
 
     window.addEventListener("keyup", function (e) {
         e.preventDefault();
-        loop.changeKeyPressed(e.keyCode, false);
+        engine.changeKeyPressed(e.keyCode, false);
     }, false);
 
     // Render the first frame
-    loop.draw(canvas, loop.map, loop.player, loop.samples, loop.pixel);
+    engine.draw(canvas, engine.player, engine.map, engine.samples, engine.pixel);
 
     // Now loop
     setInterval(function () {
-        if (loop.move(loop.player, loop.map)) {
-            loop.draw(canvas, loop.map, loop.player, loop.samples, loop.pixel);
+        if (engine.move(engine.player, engine.map)) {
+            engine.draw(canvas, engine.player, engine.map, engine.samples, engine.pixel);
         }
     }, 50);
 
